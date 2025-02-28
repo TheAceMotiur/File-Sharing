735ead5bf1670045591b2f6bddd830aa9512070d
TheAceMotiur <motiurop@outlook.com>
2025-02-28 11:34:50 +0300

    Add PDO connection setup for database interaction

---
+++ pdo.php
@@ -0,0 +1,20 @@
+<?php
+$host = '184.164.80.178';
+$db   = 'onenetly_home';
+$user = 'onenetly_home';
+$pass = 'AmiMotiur27@';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
