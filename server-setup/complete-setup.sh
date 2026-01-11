d0cbb3a89d6f8376ed790317a4e18b69eea2f097
Motiur Rahman <90847178+TheAceMotiur@users.noreply.github.com>
2026-01-11 17:34:48 +0300

    Remove redundant PHP 8.2 package installation from setup script

--- server-setup/complete-setup.sh
+++ server-setup/complete-setup.sh
@@ -122,7 +122,7 @@ add-apt-repository -y ppa:ondrej/php
 apt update
 apt install -y php8.2 php8.2-fpm php8.2-cli php8.2-common php8.2-mysql \
     php8.2-zip php8.2-gd php8.2-mbstring php8.2-curl php8.2-xml php8.2-bcmath \
-    php8.2-json php8.2-intl php8.2-soap php8.2-imagick php8.2-opcache php8.2-redis
+    php8.2-intl php8.2-soap php8.2-imagick php8.2-opcache php8.2-redis
 systemctl start php8.2-fpm
 systemctl enable php8.2-fpm
 a2enconf php8.2-fpm
